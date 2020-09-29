d1da1506a33aa74edf377f5385ddb8b19207a839
stasenkots <tstasenko2@gmail.com>
2020-09-29 16:42:16 +0300

    update

--- app/src/main/java/com/example/tms/countries/Country.kt
+++ app/src/main/java/com/example/tms/countries/Country.kt
@@ -1,26 +1,25 @@
 package com.example.tms.countries
 
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlin.random.Random
 
 
-
-interface Country{
-    var potato:MutableLiveData<Int>
-    var cabbage:MutableLiveData<Int>
-    var beet:MutableLiveData<Int>
-    val name:String
-
+class Country(val name: String) {
+    var potato = MutableLiveData(0)
+    var cabbage = MutableLiveData(0)
+    var beet = MutableLiveData(0)
     suspend fun reapCrop() {
-        while (potato.value?.plus(cabbage.value ?:0)?.plus(beet.value?:0)?:0 < 100){
-        potato.postValue( potato.value?.plus(Random.Default.nextInt(10)))
-        cabbage.postValue(cabbage.value?.plus(Random.Default.nextInt(10)))
-        beet.postValue(beet.value?.plus(Random.Default.nextInt(10)))
-        delay(1000)
-    }
-    }
+        while (potato.value?.plus(cabbage.value ?: 0)?.plus(beet.value ?: 0) ?: 0 < 100) {
+            potato.postValue(potato.value?.plus(Random.Default.nextInt(10)))
+            cabbage.postValue(cabbage.value?.plus(Random.Default.nextInt(10)))
+            beet.postValue(beet.value?.plus(Random.Default.nextInt(10)))
+            delay((Random.Default.nextInt()%2000).toLong())
+        }
 
-
+    }
 }
+
+
+
 
--- app/src/main/java/com/example/tms/countries/MyViewModel.kt
+++ app/src/main/java/com/example/tms/countries/MyViewModel.kt
@@ -9,19 +9,26 @@ import kotlinx.coroutines.*
 
 
 class MyViewModel : ViewModel() {
-    private val countries = arrayListOf(MinskCountry, BrestCountry, GomelCountry, GrodnoCountry)
+    val countries= mapOf(
+        "MinskCountry" to Country("MinskCountry"),
+        "GomelCountry" to Country("GomelCountry"),
+        "GrodnoCountry" to Country("GrodnoCountry"),
+        "BrestCountry" to Country("BrestCountry")
+    )
     @Volatile var winner=MutableLiveData<Country>()
-    fun findWinnerInReapingCrop(){
-        for (country in countries) {
-           CoroutineScope(Dispatchers.IO).launch {
-                country.reapCrop()
-            }.invokeOnCompletion {
-               if (winner.value==null){
-                   winner.postValue(country)
-               }
-           }
+    @InternalCoroutinesApi
+    fun findWinnerInReapingCrop()= CoroutineScope(Dispatchers.IO).launch{
+        for (country in countries.values) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    country.reapCrop()
+                }.invokeOnCompletion{
+                    kotlinx.coroutines.internal.synchronized(this) {
+                        if (winner.value== null){
+                            winner.postValue(country)
+                        }
+                    }
+                }
         }
-
     }
 }
 
