fe2d29bebfcd8172417cf81f56b069bbccdd4204
stasenkots <tstasenko2@gmail.com>
2020-09-29 16:52:41 +0300

    fixed  findWinnerInReapingCrop

--- app/src/main/java/com/example/tms/countries/MyViewModel.kt
+++ app/src/main/java/com/example/tms/countries/MyViewModel.kt
@@ -9,25 +9,27 @@ import kotlinx.coroutines.*
 
 
 class MyViewModel : ViewModel() {
-    val countries= mapOf(
+    val countries = mapOf(
         "MinskCountry" to Country("MinskCountry"),
         "GomelCountry" to Country("GomelCountry"),
         "GrodnoCountry" to Country("GrodnoCountry"),
         "BrestCountry" to Country("BrestCountry")
     )
-    @Volatile var winner=MutableLiveData<Country>()
+    @Volatile
+    var winner = MutableLiveData<Country>()
+
     @InternalCoroutinesApi
-    fun findWinnerInReapingCrop()= CoroutineScope(Dispatchers.IO).launch{
+    fun findWinnerInReapingCrop() {
         for (country in countries.values) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    country.reapCrop()
-                }.invokeOnCompletion{
-                    kotlinx.coroutines.internal.synchronized(this) {
-                        if (winner.value== null){
-                            winner.postValue(country)
-                        }
+            CoroutineScope(Dispatchers.IO).launch {
+                country.reapCrop()
+            }.invokeOnCompletion {
+                kotlinx.coroutines.internal.synchronized(this) {
+                    if (winner.value == null) {
+                        winner.postValue(country)
                     }
                 }
+            }
         }
     }
 }
